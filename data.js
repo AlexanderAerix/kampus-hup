2a6764a57953d0b3a491e2271d4fb46b518203a0
Alexander Aerix <theakuarif@gmail.com>
2026-09-14 20:35:13 +0700

    add style and javascript

---
+++ data.js
@@ -0,0 +1,32 @@
+export const dataA = [
+  {
+    nama: "alex",
+    smester: 3,
+    prodi: "teknik informatika",
+    ipk: 4,
+  },
+  {
+    nama: "saipul",
+    smester: 3,
+    prodi: "teknik informatika",
+    ipk: 4,
+  },
+  {
+    nama: "asep",
+    smester: 3,
+    prodi: "teknik informatika",
+    ipk: 4,
+  },
+  {
+    nama: "ucup",
+    smester: 3,
+    prodi: "teknik informatika",
+    ipk: 4,
+  },
+  {
+    nama: "rafia",
+    smester: 3,
+    prodi: "teknik informatika",
+    ipk: 4,
+  },
+];
---
+++ main.js
@@ -0,0 +1,2 @@
+import { dataA } from "./data.js";
+console.log(dataA[0]); // "alex"
